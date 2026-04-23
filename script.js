85db1a407cc2f91322f306740936fd4f027bbcc5
aryanjain28 <aryanjn807@gmail.com>
2026-04-22 22:01:11 -0400

    changed days to 2 digits

--- script.js
+++ script.js
@@ -60,7 +60,7 @@
         const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-        document.getElementById('days').textContent = String(days).padStart(3, '0');
+        document.getElementById('days').textContent = String(days).padStart(2, '0');
         document.getElementById('hours').textContent = String(hours).padStart(2, '0');
         document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
         document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
